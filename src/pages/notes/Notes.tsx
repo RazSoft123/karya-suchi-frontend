84ae0c327cb15b45d38ccb16c73723d67df6e253
RazSoft <razgroupgame@gmail.com>
2026-08-20 00:02:56 +0530

    FEATURE: Update Notes page header and content; display notes using NoteCard component

--- src/pages/notes/Notes.tsx
+++ src/pages/notes/Notes.tsx
@@ -1,12 +1,23 @@
+import NoteCard from "../../components/notes/NoteCard";
+import { fakeNotes } from "../../fakeData";
+
 export default function Notes() {
   return (
     <main className="font-inter px-4 py-4 max-h-dvh overflow-scroll">
       <div className="flex flex-col gap-6">
         <section>
-          <h1 className="font-inter text-2xl font-semibold">Good Morning,</h1>
+          <h1 className="font-inter text-2xl font-semibold">Notes</h1>
           <p className="font-inter text-sm text-slate-600">
-            You have 6 tasks due today, 2 overdue tasks, 3 upcoming deadlines
+            All your notes at one place
           </p>
+        </section>
+        <section></section>
+        <section>
+          <div className="w-full grid grid-cols-4 gap-4">
+            {fakeNotes.map((note) => {
+              return <NoteCard key={note.id} note={note} />;
+            })}
+          </div>
         </section>
       </div>
     </main>
